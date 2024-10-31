1265b769ec703d5de3d0730cff761800a0ce6802
Dxmien <brightobesi@gmail.com>
2024-10-31 18:25:44 +0100

    cart functionaility

--- lib/checkout.dart
+++ lib/checkout.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_app/global_variable.dart';
 
-class Checkout extends StatelessWidget {
+class Checkout extends StatefulWidget {
   final List<Map<String, dynamic>> cart;
   // final String imageName;
   // final String price;
@@ -14,36 +14,73 @@ class Checkout extends StatelessWidget {
       required this.cart});
 
   @override
+  State<Checkout> createState() => _CheckoutState();
+}
+
+class _CheckoutState extends State<Checkout> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text("Cart"),
         ),
-        body: Center(
-          child: ListView.builder(
-            itemCount: cart.length,
-            itemBuilder: (context, index) {
-              final item = cart[index];
-              // final title = item['title'] as String;
-              // print(item['title']);
-              return Card(
-                child: Center(
-                  child: Column(
-                    children: [
-                      Text(item['title']),
-                      Image(
-                        image: AssetImage(item['imageurl']),
+        body: Stack(children: [
+          Center(
+            child: ListView.builder(
+              itemCount: widget.cart.length,
+              itemBuilder: (context, index) {
+                final item = widget.cart[index];
+                // final title = item['title'] as String;
+                // print(item['title']);
+                return Card(
+                  child: Center(
+                    child: Stack(children: [
+                      Column(
+                        children: [
+                          Text(
+                            item['title'],
+                            style: Theme.of(context).textTheme.titleMedium,
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Image(
+                            image: AssetImage(item['imageurl']),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            "\$${item['price']}",
+                            style: Theme.of(context).textTheme.titleMedium,
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                        ],
                       ),
-                      Text(
-                        "\$${item['price']}",
-                        style: Theme.of(context).textTheme.titleMedium,
-                      )
-                    ],
+                    ]),
                   ),
-                ),
-              );
-            },
+                );
+              },
+            ),
           ),
-        ));
+          Positioned(
+            bottom: 30, // Distance from bottom
+            right: 25, // Distance from right
+            child: FloatingActionButton(
+              backgroundColor: const Color.fromARGB(255, 255, 217, 30),
+              child: Icon(
+                Icons.delete,
+                color: Colors.black,
+              ),
+              onPressed: () {
+                widget.cart.clear();
+                setState(() {});
+                // Define your onPressed functionality here
+              },
+            ),
+          ),
+        ]));
   }
 }
